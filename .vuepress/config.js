14ab3605f26c81f8ef8a7b4b12031a93b86b0ade
Barnaby Keene <hello@southcla.ws>
2019-03-03 13:12:18 +0000

    fixed invalid dest
    Former-commit-id: 748240937f95e40378146944be71e06d17814964

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "SA:MP API Wiki",
   description: "Documentation for SA:MP APIs and server configuration.",
-  dest: ".vuepress/config.js",
+  dest: "public",
   themeConfig: {
     logo: "/logo.gif",
     sidebar: "auto",
