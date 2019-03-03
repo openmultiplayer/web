68cb0ff9a4994e5b24c8b8dfec1adb312fafe621
Barnaby Keene <hello@southcla.ws>
2019-03-03 13:16:03 +0000

    add base
    Former-commit-id: f03ef9966e475837371f3d99097569135d25f2ad

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: "SA:MP API Wiki",
   description: "Documentation for SA:MP APIs and server configuration.",
+  base: "/samp-wiki/",
   dest: "public",
   themeConfig: {
     logo: "/logo.gif",
