b5270c7caedb1606b6d637df997a5ef6602ee497
Southclaws <accounts@southcla.ws>
2020-11-16 17:11:40 +0000

    add a simple remark plugin for text colouring
    Former-commit-id: 6e556bb18dc61c53924ab18d46d2cedbaa657620

--- frontend/pages/docs/[...path].tsx
+++ frontend/pages/docs/[...path].tsx
@@ -23,6 +23,71 @@ import { readFileSync } from "fs";
 import { resolve } from "path";
 import { GetStaticPropsContext } from "next";
 import hydrate from "next-mdx-remote/hydrate";
+import visit from "unist-util-visit";
+
+// Remark plugin for colours.
+// Usage:
+//
+// {text}(#hexcode)
+//
+// This is {yellow}(#ffff00) text!
+//
+
+const colourPattern = /{(.+?)}\(#([a-f0-9]{6})\)/gm;
+
+const remarkColour = function attacher() {
+  return (tree) => {
+    visit(tree, "paragraph", (node) => {
+      let children: Array<any> = node.children as Array<any>;
+      let newChildren: Array<any> = node.children as Array<any>;
+
+      children.map((v: { value: string }, i: number) => {
+        let match: RegExpExecArray;
+        let insertions: number = 0;
+        let lastIndex = 0;
+        do {
+          match = colourPattern.exec(v.value);
+          if (match?.length !== 3) {
+            continue;
+          }
+
+          newChildren.splice(
+            i + insertions,
+            0,
+            {
+              type: "text",
+              value: v.value.slice(lastIndex, match.index),
+            },
+            {
+              type: "jsx",
+              value: `<span style={{color: "#${match[2]}"}}>`,
+            },
+            {
+              type: "text",
+              value: match[1],
+            },
+            {
+              type: "jsx",
+              value: `</span>`,
+            }
+          );
+          insertions += 4;
+
+          lastIndex = match.index + match[0].length;
+        } while (match);
+
+        if (insertions > 0) {
+          // remove the original text node and insert the remaining text
+          newChildren.splice(i + insertions, 1, {
+            type: "text",
+            value: v.value.slice(lastIndex, v.value.length),
+          });
+        }
+      });
+      node.children = newChildren;
+    });
+  };
+};
 
 export async function getStaticProps(
   context: GetStaticPropsContext<{ path: string[] }>
@@ -34,7 +99,11 @@ export async function getStaticProps(
 
   // TODO: plugins for admonitions and frontmatter etc
   // also, pawn syntax highlighting
-  const mdxSource = await renderToString(source);
+  const mdxSource = await renderToString(source, {
+    mdxOptions: {
+      remarkPlugins: [remarkColour],
+    },
+  });
 
   return { props: { source: mdxSource } };
 }
