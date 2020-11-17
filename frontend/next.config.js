fe9975e5274d7a937c67ea21d9d7f01701369d3c
Southclaws <accounts@southcla.ws>
2020-11-17 21:05:30 +0000

    generate list of locales

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports = {
   i18n: {
-    locales: ["en", "fr", "nl"],
+    locales: fs
+      .readdirSync("content")
+      .filter((v) => fs.statSync(path.join("content", v)).isDirectory())
+      .map((v) => v),
     defaultLocale: "en",
   },
 };
