77b1e390192337472156bea52b00125b84f07474
Southclaws <accounts@southcla.ws>
2020-11-18 00:10:24 +0000

    removed the fixed nonsense
    Former-commit-id: 857f13a0ac66cd5bc03ca0b50e9edaa5b53fa9bd

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import { WordmarkInverse } from "../components/logo";
+import { Wordmark } from "../components/logo";
 import hydrate from "next-mdx-remote/hydrate";
 
 type Props = {
@@ -9,62 +8,16 @@ type Props = {
 const Page = ({ content }: Props) => (
   <>
     <section className="pa0 ma0">
-      <div className="foreground flex">
-        <WordmarkInverse />
+      <div>
+        <Wordmark />
       </div>
 
-      <article className="pa0 ma0 bg-white">
+      <article className="pa0 ma0">
         <div className="center measure pa4">
           {hydrate(content, { components: MDX_COMPONENTS })}
         </div>
       </article>
-
-      <div className="background gradient">
-        <Image
-          className="fixed"
-          src="/img/hero-bg.jpg"
-          width={1660}
-          height={560}
-          layout="responsive"
-        />
-      </div>
     </section>
-
-    <style jsx>{`
-      section {
-        display: grid;
-        grid-template-rows: 4em auto;
-      }
-      .foreground {
-        grid-row: 2/3;
-        grid-column: 1/2;
-        z-index: 2;
-      }
-      .background {
-        z-index: 0;
-        height: 100%;
-        width: 100%;
-        position: fixed;
-        max-height: 80vh;
-        overflow: clip;
-        top: 1;
-      }
-
-      .gradient {
-        background: rgb(0, 0, 0);
-        background: linear-gradient(
-          0deg,
-          rgba(0, 0, 0, 1) 0%,
-          rgba(0, 0, 0, 1) 60%,
-          rgba(0, 0, 0, 0) 100%
-        );
-      }
-      article {
-        grid-row: 3/4;
-        grid-column: 1/2;
-        z-index: 2;
-      }
-    `}</style>
   </>
 );
 
