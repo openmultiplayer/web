4edf299d2e1356c3608ab8f3a83ec205e212eac7
Southclaws <accounts@southcla.ws>
2020-11-19 13:20:23 +0000

    add a form to the servers list for adding servers

--- frontend/pages/servers/index.tsx
+++ frontend/pages/servers/index.tsx
@@ -208,6 +208,44 @@ const List = ({ data }: { data: Array<Server> }) => {
           </button>
         </span> */}
       </form>
+      <form
+        action={API_SERVERS}
+        target="_self"
+        method="post"
+        className="flex flex-wrap items-center justify-center pa2 f7"
+      >
+        <span className={formItemStyle}>
+          <label className="pr2 self-center" htmlFor="address">
+            Add Server:
+          </label>
+          <input
+            className="pr2"
+            type="text"
+            name="address"
+            placeholder="IP or Domain"
+          />
+          <input
+            className={[
+              "ph3",
+              "f6",
+              "pointer",
+              "no-underline",
+              "black",
+              "bg-white",
+              "hover-bg-light-red",
+              "hover-white",
+              "inline-flex",
+              "items-center",
+              "pa2",
+              "ba",
+              "border-box",
+              "mr4",
+            ].join(" ")}
+            type="submit"
+            value="Add"
+          />
+        </span>
+      </form>
       <ul className="list pl0 mt0 center">
         {dataToList(data, {
           search,
