002fcf8d94c7385b179573511d11bce287be67a5
Southclaws <accounts@southcla.ws>
2020-11-19 13:18:57 +0000

    use form data parsing for adding IPs

--- server/src/api/servers/h_add.go
+++ server/src/api/servers/h_add.go
@@ -4,23 +4,26 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/pkg/errors"
+
 	"github.com/openmultiplayer/web/server/src/server"
 	"github.com/openmultiplayer/web/server/src/web"
-	"github.com/pkg/errors"
 )
 
-type addPayload struct {
-	IP string `json:"ip"`
-}
-
 func (s *service) add(w http.ResponseWriter, r *http.Request) {
-	p := addPayload{}
-	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
-		web.StatusBadRequest(w, errors.Wrap(err, "failed to decode body as JSON"))
+	address := r.FormValue("address")
+	if address == "" {
+		web.StatusBadRequest(w, errors.New("no address specified"))
 		return
 	}
 
-	ss := server.HydrateDomain(r.Context(), server.TransformQueryResult(s.queryer.Query(r.Context(), p.IP)))
+	normalised, errs := server.AddressFromString(address)
+	if errs != nil {
+		web.StatusBadRequest(w, errors.Errorf("%v", errs))
+		return
+	}
+
+	ss := server.HydrateDomain(r.Context(), server.TransformQueryResult(s.queryer.Query(r.Context(), normalised)))
 	if ss.IP == "" {
 		web.StatusNotFound(w, errors.New("The specified IP did not resolve to a SA-MP server."))
 		return
