19a31bc4e5cc4a7d0bc874125e2b4d4de818abaa
Southclaws <accounts@southcla.ws>
2020-12-09 19:33:25 +0000

    implement logout route

---
+++ frontend/src/pages/logout.tsx
@@ -0,0 +1,15 @@
+import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
+
+const Page = () => null;
+
+export const getServerSideProps = async (
+  ctx: GetServerSidePropsContext
+): Promise<GetServerSidePropsResult<{}>> => {
+  ctx.res.setHeader("clear-site-data", '"cookies"');
+  ctx.res.writeHead(302, { Location: "/dashboard" });
+  ctx.res.end();
+
+  return { props: {} };
+};
+
+export default Page;
