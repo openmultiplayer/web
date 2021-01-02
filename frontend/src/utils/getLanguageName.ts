3ce89c5ba002959a7839b2fa81b80f7d5e45f8a5
Kamirai <kami@sostm.org>
2021-01-02 21:54:14 +0900

    Try to let both Chinese languages display correct.
    has been tested

--- frontend/src/utils/getLanguageName.ts
+++ frontend/src/utils/getLanguageName.ts
@@ -1,8 +1,8 @@
 import ISO6391 from "iso-639-1";
 
 const missingLanguages = {
-  "zh-TW": "正體中文/繁體中文",
-  "zh-CN": "简化字",
+  "zh-tw": "繁體中文",
+  "zh-cn": "简体中文",
 };
 
 const getLanguageName = (short: string) =>
