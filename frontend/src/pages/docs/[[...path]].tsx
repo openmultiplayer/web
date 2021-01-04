9d9b17de4c1a60660d7656fd415fbecd75949135
Southclaws <accounts@southcla.ws>
2021-01-04 16:23:36 +0000

    🤠

--- frontend/src/pages/docs/[[...path]].tsx
+++ frontend/src/pages/docs/[[...path]].tsx
@@ -169,6 +169,8 @@ export async function getStaticPaths() {
     filterFn = buildOnlyLargePages(60000);
   }
 
+  console.log("ENV:", process.env); // TODO: Remove!
+
   const paths = glob
     .sync("../docs/meta/*.md") // read docs from the repo root
     .filter(filterFn)
