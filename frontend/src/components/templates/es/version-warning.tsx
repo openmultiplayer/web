09701d890ed3dc53ec4f9ff1d1e8debbde406a4f
PW-Roman <78453332+PW-Roman@users.noreply.github.com>
2021-02-06 20:33:53 -0300

    Update version-warning.tsx

--- frontend/src/components/templates/es/version-warning.tsx
+++ frontend/src/components/templates/es/version-warning.tsx
@@ -10,8 +10,8 @@ export default function WarningVersion({
   return (
     <Admonition type="warning">
       <p>
-        Esta {name} foi implementada no {version} e não funcionará em
-        versões anteriores.
+        Esta {name} fue implementada en {version} y no funcionará en
+        versiones anteriores.
       </p>
     </Admonition>
   );
