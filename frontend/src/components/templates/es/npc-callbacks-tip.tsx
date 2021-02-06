2b61b8fd0535cf46ab89d1ffa1643df92e7b2b87
PW-Roman <78453332+PW-Roman@users.noreply.github.com>
2021-02-06 20:35:10 -0300

    Update npc-callbacks-tip.tsx

--- frontend/src/components/templates/es/npc-callbacks-tip.tsx
+++ frontend/src/components/templates/es/npc-callbacks-tip.tsx
@@ -3,7 +3,7 @@ import Admonition from "../../Admonition";
 export default function TipNpcCallback() {
   return (
     <Admonition type="tip">
-      <p>Esta callback também pode ser chamada por um NPC.</p>
+      <p>Este callback también puede ser llamado por un NPC.</p>
     </Admonition>
   );
 }
