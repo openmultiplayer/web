907c6b32d5e70d38cf904119d57da506053623d6
Barnaby Keene <accounts@southcla.ws>
2021-08-08 12:35:50 +0100

    add quick and dirty dev login form

---
+++ frontend/src/pages/auth/dev.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="measure center pa4 ">
+      <form className="flex" action={"http://localhost/users/dev"} method="get">
+        <input type="text" name="id" id="id" placeholder="id" />
+        <input type="text" name="secret" id="secret" placeholder="secret" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Page;
