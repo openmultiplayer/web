79079ea3e483d320d308f9583cdab2d212c25041
Barnaby Keene <accounts@southcla.ws>
2021-08-18 19:42:16 +0100

    comment out quick hacky test code that isn't part of the test suite

--- server/src/resources/forum/db_test.go
+++ server/src/resources/forum/db_test.go
@@ -1,49 +1,42 @@
 package forum
 
-import (
-	"context"
-	"fmt"
-	"os"
-	"testing"
+// NOTE: Quick and dirty "tests" not part of test suite.
+//
+// func TestForum(t *testing.T) {
+// 	os.Setenv("DATABASE_URL", "postgresql://default:default@localhost:5432/prisma")
 
-	"github.com/openmultiplayer/web/server/src/db"
-)
+// 	prisma := db.NewClient()
+// 	if err := prisma.Connect(); err != nil {
+// 		panic(err)
+// 	}
 
-func TestForum(t *testing.T) {
-	os.Setenv("DATABASE_URL", "postgresql://default:default@localhost:5432/prisma")
+// 	user, err := prisma.User.
+// 		UpsertOne(
+// 			db.User.Email.Equals("one@user.com"),
+// 		).
+// 		Create(
+// 			db.User.Email.Set("one@user.com"),
+// 			db.User.AuthMethod.Set(db.AuthMethodDISCORD),
+// 			db.User.Name.Set("bob"),
+// 		).
+// 		Update(
+// 		//
+// 		).
+// 		Exec(context.Background())
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
 
-	prisma := db.NewClient()
-	if err := prisma.Connect(); err != nil {
-		panic(err)
-	}
+// 	fmt.Println(user)
 
-	user, err := prisma.User.
-		UpsertOne(
-			db.User.Email.Equals("one@user.com"),
-		).
-		Create(
-			db.User.Email.Set("one@user.com"),
-			db.User.AuthMethod.Set(db.AuthMethodDISCORD),
-			db.User.Name.Set("bob"),
-		).
-		Update(
-		//
-		).
-		Exec(context.Background())
-	if err != nil {
-		fmt.Println(err)
-	}
+// 	forum := New(prisma)
 
-	fmt.Println(user)
+// 	thread, err := forum.CreateThread(context.Background(), "First thread", "this is my thread", user.ID, []string{})
+// 	fmt.Println(thread, err)
 
-	forum := New(prisma)
+// 	post, err := forum.CreatePost(context.Background(), "this is my post", user.ID, thread.ID)
+// 	fmt.Println(post, err)
 
-	thread, err := forum.CreateThread(context.Background(), "First thread", "this is my thread", user.ID, []string{})
-	fmt.Println(thread, err)
-
-	post, err := forum.CreatePost(context.Background(), "this is my post", user.ID, thread.ID)
-	fmt.Println(post, err)
-
-	post, err = forum.CreatePost(context.Background(), "this is my second post\nisn't it cool!!!!!\nwow!!!", user.ID, thread.ID)
-	fmt.Println(post, err)
-}
+// 	post, err = forum.CreatePost(context.Background(), "this is my second post\nisn't it cool!!!!!\nwow!!!", user.ID, thread.ID)
+// 	fmt.Println(post, err)
+// }
