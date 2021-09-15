7f3f25de22dc861624ac8ba4efdca1a3e867ef65
iAmir <amirabgta@gmail.com>
2021-09-16 03:12:20 +0430

    Update _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -166,6 +166,7 @@ const App = ({ Component, pageProps, router }: AppProps) => (
           items={[
             { name: "Home", path: "/", exact: true },
             { name: "FAQ", path: "/faq" },
+            { name: "Forums", path: "https://burgershot.gg/" },
             { name: "Servers", path: "/servers" },
             { name: "Docs", path: "/docs" },
             { name: "Blog", path: "/blog" },
