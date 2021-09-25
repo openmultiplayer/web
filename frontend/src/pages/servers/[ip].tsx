011e75e397b08baef602af127b8ac8d911bcb8d4
Barnaby Keene <accounts@southcla.ws>
2021-09-25 14:33:56 +0000

    spiced up the server page

--- frontend/src/pages/servers/[ip].tsx
+++ frontend/src/pages/servers/[ip].tsx
@@ -1,6 +1,20 @@
-import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
+import { Button } from "@chakra-ui/button";
+import { useColorModeValue } from "@chakra-ui/color-mode";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { Box, Flex, Link, Stack, Text } from "@chakra-ui/layout";
+import { Spinner } from "@chakra-ui/spinner";
+import { Stat, StatLabel, StatNumber } from "@chakra-ui/stat";
+import { chakra } from "@chakra-ui/system";
+import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
+import {
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+  NextPage,
+} from "next";
 import { NextSeo } from "next-seo";
-import Link from "next/link";
+import NextLink from "next/link";
+import { FC } from "react";
+import { APIError } from "src/types/_generated_Error";
 import { All } from "src/types/_generated_Server";
 import useSWR from "swr";
 
@@ -30,37 +44,6 @@ const Item = ({ k, v }: { k: string; v: string }) => {
 
 type ServerProp = { server: All };
 
-const Description = ({ server }: ServerProp) => {
-  if (!server.description) {
-    return null;
-  }
-  return (
-    <>
-      <h4>Description</h4>
-      <p>{server.description}</p>
-    </>
-  );
-};
-
-const Properties = ({ server }: ServerProp) => {
-  if (!server.core) {
-    return null;
-  }
-  return (
-    <>
-      <h4>Properties:</h4>
-      <dl className="lh-title ph4 mt0">
-        <Item k="Address" v={server.core.ip} />
-        <Item k="Hostname" v={server.core.hn} />
-        <Item k="Players" v={`${server.core.pc}/${server.core.pm}`} />
-        <Item k="Gamemode" v={server.core.gm} />
-        <Item k="Language" v={server.core.la} />
-        <Item k="Mod Version" v={server.core.vn} />
-      </dl>
-    </>
-  );
-};
-
 const Rules = ({ server }: ServerProp) => {
   if (!server.ru) {
     return null;
@@ -85,45 +68,157 @@ const Info = ({ data }: { data: All }) => (
       description={`${data.core.pc}/${data.core.pm} players currently online at ${data.core.hn} (${data.core.gm}). View more information on open.mp servers list.`}
     />
 
-    <hgroup>
-      <h1 className="pv0 mb0">{data.core.hn}</h1>
-      <div className="black-50 pv1 mt0 flex justify-between">
-        <h2 className="di static mv0">{data.core.gm}</h2>
-        <span className="lh-copy f6 self-end">
-          {data.core.pa ? "🔐" : null}
-          {data.core.ip}
-        </span>
-      </div>
-    </hgroup>
+    <Flex alignItems="center" justifyContent="center">
+      <Box
+        px={8}
+        py={4}
+        rounded="lg"
+        shadow="lg"
+        bg={useColorModeValue("white", "gray.800")}
+      >
+        <Stack spacing="4">
+          <Flex justifyContent="space-between" alignItems="center">
+            <chakra.span
+              fontSize="sm"
+              color={useColorModeValue("gray.600", "gray.400")}
+            >
+              {data.core.gm}
+            </chakra.span>
+            <Link
+              href={`samp://${data.dm ?? data.ip}`}
+              isExternal
+              px={3}
+              py={1}
+              bg="gray.600"
+              color="gray.100"
+              fontSize="sm"
+              fontWeight="700"
+              rounded="md"
+              _hover={{ bg: "gray.500" }}
+            >
+              {data.dm ?? data.ip}
+            </Link>
+          </Flex>
 
-    <section>
-      <Description server={data} />
-      <Properties server={data} />
-      <Rules server={data} />
-    </section>
+          <Box mt={2}>
+            <Link
+              fontSize="2xl"
+              color={useColorModeValue("gray.700", "white")}
+              fontWeight="700"
+              _hover={{
+                color: useColorModeValue("gray.600", "gray.200"),
+                textDecor: "underline",
+              }}
+            >
+              {data.core.hn}
+            </Link>
+          </Box>
+
+          <Box>
+            {data.description ? (
+              <Text>{data.description}</Text>
+            ) : (
+              <Text color="teal">This server has no description</Text>
+            )}
+          </Box>
+
+          <Stack
+            spacing="4"
+            direction={["column", "row"]}
+            justifyContent="space-around"
+          >
+            <Flex direction="column" justify="space-around" align="center">
+              <Stat>
+                <StatLabel>Players Online</StatLabel>
+                <StatNumber>
+                  {data.core.pc}/{data.core.pm}
+                </StatNumber>
+              </Stat>
+
+              <Stat>
+                <StatLabel>Mod Version</StatLabel>
+                <StatNumber>{data.core.vn}</StatNumber>
+              </Stat>
+
+              <Stat>
+                <StatLabel>Language</StatLabel>
+                <StatNumber>{data.core.la}</StatNumber>
+              </Stat>
+            </Flex>
+
+            <Box>
+              {data.ru ? (
+                <Table variant="striped" width="full" size="sm">
+                  <Thead>
+                    <Tr>
+                      <Th>Rule</Th>
+                      <Th>Value</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {Object.keys(data.ru).map((v) => (
+                      <Tr key={v}>
+                        <Td>{v}</Td>
+                        <Td>{data.ru[v]}</Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
+              ) : null}
+            </Box>
+          </Stack>
+        </Stack>
+      </Box>
+    </Flex>
   </article>
 );
 
-const Error = ({ error }: { error: TypeError }) => (
+const Content = ({ ip, initialData }: Props) => {
+  const { data, error } = useSWR<All, TypeError>(ip, getServer, {
+    initialData,
+  });
+  if (error) {
+    return <Error error={error} />;
+  }
+  if (!data) {
+    return (
+      <Flex justify="center" width="full">
+        <Spinner size="xl" />
+      </Flex>
+    );
+  }
+
+  return <Info data={data} />;
+};
+
+const Error: FC<{ error: APIError }> = ({ error }) => (
   <p>
     Unfortunately there was an error while getting the server list! The error
     message is below: <pre>{error.message}</pre>
   </p>
 );
-const Page = ({ ip, initialData }: Props) => {
-  const { data, error } = useSWR<All, TypeError>(ip, getServer, {
-    initialData,
-  });
-
+const Page: NextPage<Props> = ({ ip, initialData }) => {
   return (
     <>
-      <section className="measure-wide center">
-        <Link href="/servers">
-          <a className="black-80 hover-blue">Back</a>
-        </Link>
+      <section className="center mv3">
+        <Stack direction={["column"]} spacing="4">
+          <NextLink href="/servers">
+            <Link className="black-80 hover-blue">
+              <Button leftIcon={<ArrowBackIcon />} variant="outline">
+                Back
+              </Button>
+            </Link>
+          </NextLink>
 
-        {error ? <Error error={error} /> : <Info data={data!} />}
+          <Content ip={ip} initialData={initialData} />
+        </Stack>
       </section>
+
+      <style jsx>{`
+        section {
+          max-width: 50em;
+        }
+      `}</style>
     </>
   );
 };
