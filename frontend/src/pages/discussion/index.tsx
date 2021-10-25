736f81ec04ace9bbf2c31188e8cf793b0d38978d
Barnaby Keene <accounts@southcla.ws>
2021-10-25 22:01:37 +0100

    reading wrong query param for text queries

--- frontend/src/pages/discussion/index.tsx
+++ frontend/src/pages/discussion/index.tsx
@@ -4,25 +4,25 @@ import ThreadView from "src/components/forum/ThreadView";
 
 const Page: NextPage = () => {
   const {
-    query: { text, tags },
+    query: { query, tags },
   } = useRouter();
 
   return (
     <ThreadView
       initialTags={tags !== "" ? (tags as string)?.split(",") : []}
-      initialText={text as string}
+      initialText={query as string}
     />
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const tags = ctx.query?.["tags"] as string;
-  const text = ctx.query?.["text"] as string;
+  const query = ctx.query?.["query"] as string;
 
   return {
     props: {
       tags: tags !== undefined && tags !== "" ? tags.split(",") : [],
-      text: text ?? "",
+      query: query ?? "",
     },
   };
 };
