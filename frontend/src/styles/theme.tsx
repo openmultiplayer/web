6672c0faf655359d509bcf36db8c9d02f042a588
Barnaby Keene <accounts@southcla.ws>
2021-10-26 21:12:36 +0100

    moved font to chakra/emotion font component
    added typography test page
    
    moved some styles to theme

--- frontend/src/styles/theme.tsx
+++ frontend/src/styles/theme.tsx
@@ -1,7 +1,36 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const fonts = {
-  heading: "english-grotesque, sans-serif",
+  body: `Frutiger, "Frutiger Linotype", Univers, Calibri, "Gill Sans", "Gill Sans MT", "Myriad Pro", Myriad, "DejaVu Sans Condensed", "Liberation Sans", "Nimbus Sans L", Tahoma, Geneva, "Helvetica Neue", Helvetica, Arial, sans-serif`,
+  heading: "english-grotesque",
+  mono: "Menlo, monospace",
+};
+
+const styles = {
+  global: {
+    h1: {
+      fontSize: "1.6em",
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: "1.5em",
+      fontWeight: 700,
+    },
+    h3: {
+      fontSize: "1.4em",
+      fontWeight: 300,
+    },
+    h4: {
+      fontSize: "1.3em",
+      fontWeight: 300,
+    },
+    p: {
+      marginTop: "0.5em",
+    },
+    ul: {
+      paddingLeft: "1em",
+    },
+  },
 };
 
 const colors = {
@@ -14,6 +43,6 @@ const colors = {
   },
 };
 
-const theme = extendTheme({ colors, fonts });
+const theme = extendTheme({ colors, fonts, styles });
 
 export default theme;
