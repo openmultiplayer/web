c775add8d4c0852e6e1f5149ab2827d3b841a173
Barnaby Keene <accounts@southcla.ws>
2021-11-01 12:18:57 +0000

    tweak user migration func to handle existing accounts and provide a count of migrations at the end

--- server/src/bsworker/users.go
+++ server/src/bsworker/users.go
@@ -17,14 +17,18 @@ func (w *Worker) migrateUsers(ctx context.Context) error {
 
 	zap.L().Info("migrating users", zap.Int("users", len(mybbUsers)))
 
+	count := 0
 	for _, u := range mybbUsers {
-		nu, err := w.users.CreateUser(ctx, u.Email, "LEGACY", u.Username)
+		nu, err := w.users.CreateUser(ctx, u.Email, "UNDEFINED", u.Username)
 		if err != nil {
-			zap.L().Warn("failed to migrate user", zap.String("username", u.Username), zap.Error(err))
+			zap.L().Info("failed to migrate user", zap.String("username", u.Username), zap.Error(err))
+		} else {
+			zap.L().Debug("migrated user", zap.String("username", nu.Name), zap.Int("n", count))
+			count++
 		}
-
-		zap.L().Debug("migrated user", zap.String("username", nu.Name))
 	}
+
+	zap.L().Info("finished migrating users", zap.Int("migrated", count), zap.Int("users", len(mybbUsers)))
 
 	return nil
 }
