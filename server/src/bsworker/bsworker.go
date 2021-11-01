215fde9ea17df67620d3fc3413db9b9db232ba35
Barnaby Keene <accounts@southcla.ws>
2021-11-01 12:05:57 +0000

    wtf fx?

--- server/src/bsworker/bsworker.go
+++ server/src/bsworker/bsworker.go
@@ -3,11 +3,12 @@ package bsworker
 import (
 	"context"
 
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+
 	"github.com/openmultiplayer/web/server/src/resources/burgershot"
 	"github.com/openmultiplayer/web/server/src/resources/forum"
 	"github.com/openmultiplayer/web/server/src/resources/user"
-	"go.uber.org/fx"
-	"go.uber.org/zap"
 )
 
 type Worker struct {
@@ -16,28 +17,40 @@ type Worker struct {
 	forum forum.Repository
 }
 
+func New(
+	lc fx.Lifecycle,
+	bs burgershot.Repository,
+	users user.Repository,
+	forum forum.Repository,
+) *Worker {
+	zap.L().Debug("creating bs worker")
+
+	w := &Worker{bs, users, forum}
+
+	lc.Append(fx.Hook{
+		OnStart: func(c context.Context) error {
+			zap.L().Debug("starting bs worker")
+			return w.run(c)
+		},
+	})
+
+	return w
+}
+
 func Build() fx.Option {
 	return fx.Options(
+		fx.Provide(New),
+
 		fx.Invoke(func(
 			lc fx.Lifecycle,
-			bs burgershot.Repository,
-			users user.Repository,
-			forum forum.Repository,
-		) *Worker {
-			w := &Worker{bs, users, forum}
-
-			zap.L().Debug("invoke bsworker")
-
-			lc.Append(fx.Hook{
-				OnStart: w.run,
-			})
-
-			return w
+			w *Worker,
+		) {
+			zap.L().Debug("invoking bs worker")
 		}),
 	)
 }
 
 func (w *Worker) run(ctx context.Context) error {
-	zap.L().Debug("onstart")
+	zap.L().Debug("running bsworker")
 	return w.migrateUsers(ctx)
 }
