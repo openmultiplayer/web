29aecb3d1b8ea20211b18f812e61650c8f18edb7
Barnaby Keene <accounts@southcla.ws>
2021-11-01 13:04:36 +0000

    add some bs forum apis for getting threads and users

--- server/src/resources/burgershot/repository.go
+++ server/src/resources/burgershot/repository.go
@@ -7,6 +7,10 @@ import (
 )
 
 type Repository interface {
-	GetThread(ctx context.Context, tid int) (interface{}, error)
+	GetForums(ctx context.Context) ([]bs.MybbForumsModel, error)
+	GetThread(ctx context.Context, tid int) ([]bs.MybbPostsModel, error)
+	GetThreads(ctx context.Context) ([]bs.MybbThreadsModel, error)
+	GetPosts(ctx context.Context, tid int) ([]bs.MybbPostsModel, error)
+	GetUserByID(ctx context.Context, uid int) (*bs.MybbUsersModel, error)
 	GetUsers(ctx context.Context) ([]bs.MybbUsersModel, error)
 }
