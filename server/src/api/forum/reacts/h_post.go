9c36469a2a22d0f5eb7523837c9c322cc9b70488
Barnaby Keene <accounts@southcla.ws>
2021-11-10 17:05:37 +0000

    improved invalid emoji error response

--- server/src/api/forum/reacts/h_post.go
+++ server/src/api/forum/reacts/h_post.go
@@ -36,6 +36,11 @@ func (s *service) post(w http.ResponseWriter, r *http.Request) {
 		} else if errors.Is(err, react.ErrAlreadyReacted) {
 			web.Write(w, b)
 			return
+		} else if errors.Is(err, react.ErrInvalidEmoji) {
+			web.StatusBadRequest(w, web.WithSuggestion(err,
+				"The emoji ID sent to the server was invalid.",
+				"This may be an issue with the emoji picker menu, please report this to a site administrator and include the exact emoji you tried to react with."))
+			return
 		} else {
 			web.StatusInternalServerError(w, err)
 		}
