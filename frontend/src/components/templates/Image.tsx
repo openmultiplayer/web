33b105441ec5e771bad6604d26789da570fa85ce
Barnaby Keene <accounts@southcla.ws>
2021-11-16 11:25:29 +0000

    only use assets for relative image sources

--- frontend/src/components/templates/Image.tsx
+++ frontend/src/components/templates/Image.tsx
@@ -1,5 +1,18 @@
-const Img = (props) => {
-  return <img src={`https://assets.open.mp/assets/${props.src}`} />;
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+import React, { FC } from "react";
+
+const Img: FC<
+  React.DetailedHTMLProps<
+    React.ImgHTMLAttributes<HTMLImageElement>,
+    HTMLImageElement
+  >
+> = (props) => {
+  const { src } = props;
+  if (src?.startsWith("http")) {
+    return <img {...props} />;
+  }
+  return <img src={`https://assets.open.mp/assets/${props.src}`} {...props} />;
 };
 
 export default Img;
