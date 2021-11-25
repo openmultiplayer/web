558cbaddae53f548e0643f3f435942726c47149f
Barnaby Keene <accounts@southcla.ws>
2021-11-25 12:40:36 +0000

    fix some bbcode conversions

--- server/src/bsworker/posts.go
+++ server/src/bsworker/posts.go
@@ -207,9 +207,111 @@ func (w *Worker) MigratePosts(ctx context.Context) error {
 	return nil
 }
 
+// These tags don't have a Markdown equivalent, strip them completely.
+var emptyTags = []string{
+	"font",
+	"align",
+}
+
 func bbcodeToMarkdown(bbc string) (string, error) {
 	compiler := bbcode.NewCompiler(true, true)
 	converter := md.NewConverter("", true, nil)
+
+	for _, t := range emptyTags {
+		compiler.SetTag(t, func(node *bbcode.BBCodeNode) (*bbcode.HTMLTag, bool) {
+			out := bbcode.NewHTMLTag("")
+			out.Name = ""
+			return out, true
+		})
+	}
+
+	compiler.SetTag("video", func(node *bbcode.BBCodeNode) (*bbcode.HTMLTag, bool) {
+		out := bbcode.NewHTMLTag("")
+		out.Name = "a"
+		text := bbcode.CompileText(node)
+		if len(text) > 0 {
+			out.Attrs["href"] = bbcode.ValidURL(text)
+		}
+		return out, true
+	})
+
+	compiler.SetTag("list", func(node *bbcode.BBCodeNode) (*bbcode.HTMLTag, bool) {
+		out := bbcode.NewHTMLTag("")
+		out.Name = "ul"
+		style := node.GetOpeningTag().Value
+		switch style {
+		case "a":
+			out.Attrs["style"] = "list-style-type: lower-alpha;"
+		case "A":
+			out.Attrs["style"] = "list-style-type: upper-alpha;"
+		case "i":
+			out.Attrs["style"] = "list-style-type: lower-roman;"
+		case "I":
+			out.Attrs["style"] = "list-style-type: upper-roman;"
+		case "1":
+			out.Attrs["style"] = "list-style-type: decimal;"
+		default:
+			out.Attrs["style"] = "list-style-type: disc;"
+		}
+
+		if len(node.Children) == 0 {
+			out.AppendChild(bbcode.NewHTMLTag(""))
+		} else {
+			node.Info = []*bbcode.HTMLTag{out, out}
+			tags := node.Info.([]*bbcode.HTMLTag)
+			for _, child := range node.Children {
+				curr := tags[1]
+				curr.AppendChild(node.Compiler.CompileTree(child))
+			}
+			if len(tags[1].Children) > 0 {
+				last := tags[1].Children[len(tags[1].Children)-1]
+				if len(last.Children) > 0 && last.Children[len(last.Children)-1].Name == "br" {
+					last.Children[len(last.Children)-1] = bbcode.NewHTMLTag("")
+				}
+			} else {
+				tags[1].AppendChild(bbcode.NewHTMLTag(""))
+			}
+		}
+		return out, false
+	})
+
+	compiler.SetTag("*", func(node *bbcode.BBCodeNode) (*bbcode.HTMLTag, bool) {
+		parent := node.Parent
+		for parent != nil {
+			if parent.ID == bbcode.OPENING_TAG && parent.GetOpeningTag().Name == "list" {
+				out := bbcode.NewHTMLTag("")
+				out.Name = "li"
+				tags := parent.Info.([]*bbcode.HTMLTag)
+				if len(tags[1].Children) > 0 {
+					last := tags[1].Children[len(tags[1].Children)-1]
+					if len(last.Children) > 0 && last.Children[len(last.Children)-1].Name == "br" {
+						last.Children[len(last.Children)-1] = bbcode.NewHTMLTag("")
+					}
+				} else {
+					tags[1].AppendChild(bbcode.NewHTMLTag(""))
+				}
+				tags[1] = out
+				tags[0].AppendChild(out)
+
+				if len(parent.Children) == 0 {
+					out.AppendChild(bbcode.NewHTMLTag(""))
+				} else {
+					for _, child := range node.Children {
+						curr := tags[1]
+						curr.AppendChild(node.Compiler.CompileTree(child))
+					}
+				}
+				if node.ClosingTag != nil {
+					tag := bbcode.NewHTMLTag(node.ClosingTag.Raw)
+					bbcode.InsertNewlines(tag)
+					out.AppendChild(tag)
+				}
+				return nil, false
+			}
+			parent = parent.Parent
+		}
+		return bbcode.DefaultTagCompiler(node)
+	})
 
 	html := compiler.Compile(bbc)
 	markdown, err := converter.ConvertString(html)
