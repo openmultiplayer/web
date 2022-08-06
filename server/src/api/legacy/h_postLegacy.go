1721a95103cc021ee9c5f0c1ce079e23c841a75f
Barnaby Keene <accounts@southcla.ws>
2022-08-06 12:17:23 +0100

    use cf IP if available

--- server/src/api/legacy/h_postLegacy.go
+++ server/src/api/legacy/h_postLegacy.go
@@ -18,15 +18,15 @@ func (s *service) postLegacy(w http.ResponseWriter, r *http.Request) {
 	version := chi.URLParam(r, "version")
 	port := chi.URLParam(r, "port")
 
-	address := fmt.Sprintf("%s:%s", strings.Split(r.RemoteAddr, ":")[0], port)
+	address := getConnectingIP(r, port)
 
-	zap.L().Debug("received request to index server",
+	zap.L().Info("received request to index server",
 		zap.String("version", version),
 		zap.String("address", address))
 
 	result, err := s.queryer.Query(r.Context(), address)
 	if err != nil {
-		zap.L().Debug("failed to query server", zap.Error(err))
+		zap.L().Info("failed to query server", zap.Error(err))
 		web.StatusNotAcceptable(w, errors.Wrap(err, "failed to query server"))
 		return
 	}
@@ -39,4 +39,16 @@ func (s *service) postLegacy(w http.ResponseWriter, r *http.Request) {
 	}
 
 	web.Write(w, formatted)
+}
+
+func getConnectingIP(r *http.Request, port string) string {
+	if cfip := r.Header.Get("CF-Connecting-IP"); cfip != "" {
+		return fmt.Sprintf("%s:%s", cfip, port)
+	}
+
+	if cfip := r.Header.Get("X-Forwarded-For"); cfip != "" {
+		return fmt.Sprintf("%s:%s", cfip, port)
+	}
+
+	return fmt.Sprintf("%s:%s", strings.Split(r.RemoteAddr, ":")[0], port)
 }
