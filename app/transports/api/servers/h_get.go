c44236ea662be25bb0ac654b4ef8bcb67868ca28
Barnaby Keene <accounts@southcla.ws>
2022-12-31 15:06:16 +0000

    add proper server not found error

--- app/transports/api/servers/h_get.go
+++ app/transports/api/servers/h_get.go
@@ -6,13 +6,18 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/pkg/errors"
 
+	"github.com/openmultiplayer/web/internal/db"
 	"github.com/openmultiplayer/web/internal/web"
 )
 
 func (s *service) get(w http.ResponseWriter, r *http.Request) {
 	result, err := s.storer.GetByAddress(r.Context(), chi.URLParam(r, "address"))
 	if err != nil {
-		web.StatusInternalServerError(w, errors.Wrap(err, "failed to get server"))
+		if errors.Is(err, db.ErrNotFound) {
+			web.StatusNotFound(w, err)
+		} else {
+			web.StatusInternalServerError(w, errors.Wrap(err, "failed to get server"))
+		}
 		return
 	}
 	web.Write(w, result)
