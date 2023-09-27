750f1fdc861d62528af6e5dd60570f205d264d26
iAmir <hhm6@yahoo.com>
2023-09-27 22:56:49 +0330

    redo migrations, add omp column again with default value

--- prisma/migrations/20230926174740_set_omp_default_value/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Server" ALTER COLUMN "omp" SET DEFAULT false;
--- prisma/migrations/20230927182210_remove_omp_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `omp` on the `Server` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Server" DROP COLUMN "omp";
