3d16f0e4e36d6f77975c1b71eaad2a02fbd51150
adib-yg <microsoftuser3030@gmail.com>
2024-02-19 02:35:04 +0330

    Template components translation for Turkish

---
+++ frontend/src/components/templates/tr/lowercase-note.tsx
@@ -0,0 +1,9 @@
+import Admonition from "../../Admonition";
+
+export default function NoteLowercase({ name = "function" }) {
+  return (
+    <Admonition type="warning">
+      <p>This {name} starts with a lowercase letter.</p>
+    </Admonition>
+  );
+}
---
+++ frontend/src/components/templates/tr/npc-callbacks-tip.tsx
@@ -0,0 +1,9 @@
+import Admonition from "../../Admonition";
+
+export default function TipNpcCallback() {
+  return (
+    <Admonition type="tip">
+      <p>This callback can also be called by NPC.</p>
+    </Admonition>
+  );
+}
