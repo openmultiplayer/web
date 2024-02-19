7428c8feabf8b539cdcbe8a49f83ebda52c47a6c
Ulaş Bayraktar <ulaserenbyrktr@gmail.com>
2024-02-19 12:34:19 +0300

    Update lowercase-note.tsx

--- frontend/src/components/templates/tr/lowercase-note.tsx
+++ frontend/src/components/templates/tr/lowercase-note.tsx
@@ -3,7 +3,7 @@ import Admonition from "../../Admonition";
 export default function NoteLowercase({ name = "function" }) {
   return (
     <Admonition type="warning">
-      <p>Bu {isim} küçük harfle başlar.</p>
+      <p>Bu {name} öğesi küçük harfle başlar.</p>
     </Admonition>
   );
 }
