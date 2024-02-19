5af8029d7fbcfb87574fc433da9e2efdb20825e2
Nerman <nermanlegacy@gmail.com>
2024-02-19 19:27:36 +0100

    Translate npc-callbacks-tip.tsx in to Bosnian

---
+++ frontend/src/components/templates/bs/npc-callbacks-tip.tsx
@@ -0,0 +1,9 @@
+import Admonition from "../../Admonition";
+
+export default function TipNpcCallback() {
+  return (
+    <Admonition type="tip">
+      <p>Ovaj povratni poziv također može pozvati NPC.</p>
+    </Admonition>
+  );
+}
