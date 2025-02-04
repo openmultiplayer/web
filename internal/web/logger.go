20c48bd1e476e210f3a70ed3b34fc6bcd8cf03a3
iAmir <hhm6@yahoo.com>
2025-02-04 13:51:28 +0330

    remove http logger

--- internal/web/logger.go
+++ internal/web/logger.go
@@ -2,20 +2,19 @@ package web
 
 import (
 	"net/http"
-
-	"go.uber.org/zap"
+	// "go.uber.org/zap"
 )
 
 // WithLogger is simple Zap logger HTTP middleware
 func WithLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		zap.L().Info(
-			"request",
-			zap.String("method", r.Method),
-			zap.String("path", r.URL.Path),
-			zap.Any("query", r.URL.Query()),
-			zap.Int64("body", r.ContentLength),
-		)
+		// zap.L().Info(
+		// 	"request",
+		// 	zap.String("method", r.Method),
+		// 	zap.String("path", r.URL.Path),
+		// 	zap.Any("query", r.URL.Query()),
+		// 	zap.Int64("body", r.ContentLength),
+		// )
 		next.ServeHTTP(w, r)
 	})
 }
