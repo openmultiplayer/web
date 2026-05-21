06cb43368a52eee976e98c058315769c1d93752e
itsneufox <156133096+itsneufox@users.noreply.github.com>
2026-05-21 23:00:44 +0100

    Refactor WarningVersion component to use translation for version warning messages

--- frontend/src/components/templates/version-warning.tsx
+++ frontend/src/components/templates/version-warning.tsx
@@ -1,17 +1,41 @@
 import Admonition from "../Admonition";
+import Translate, { translate } from "@docusaurus/Translate";
 
 export default function WarningVersion({
   version,
   name = "function",
 }: {
   version: string;
-  name: string;
+  name?: string;
 }) {
+  const translatedName =
+    name === "function"
+      ? translate({
+          id: "versionWarn.name.function",
+          message: "function",
+          description: "Default item name in a version warning",
+        })
+      : name === "callback"
+        ? translate({
+            id: "versionWarn.name.callback",
+            message: "callback",
+            description: "Callback item name in a version warning",
+          })
+        : name;
+
   return (
     <Admonition type="warning">
       <p>
-        This {name} was added in {version} and will not work in earlier
-        versions!
+        <Translate
+          id="versionWarn.message"
+          description="Warning shown on scripting docs pages for APIs added in a specific server version"
+          values={{
+            name: translatedName,
+            version,
+          }}
+        >
+          {"This {name} was added in {version} and will not work in earlier versions!"}
+        </Translate>
       </p>
     </Admonition>
   );
